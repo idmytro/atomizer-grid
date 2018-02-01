1a65d6cc3e4085d2554d05d46c3debaf3fb86b5d
Dmytro Cisar <i@dmyt.ro>
2018-02-02 00:09:35 +0200

    + Gcg, Grg

--- rules.js
+++ rules.js
@@ -1,6 +1,26 @@
 module.exports = [
     {
         type: 'pattern',
+        id: 'grid-column-gap',
+        name: 'grid-column-gap',
+        matcher: 'Gcg',
+        allowParamToValue: true,
+        styles: {
+            'grid-column-gap': '$0',
+        }
+    },
+    {
+        type: 'pattern',
+        id: 'grid-row-gap',
+        name: 'grid-row-gap',
+        matcher: 'Grg',
+        allowParamToValue: true,
+        styles: {
+            'grid-row-gap': '$0',
+        }
+    },
+    {
+        type: 'pattern',
         id: 'grid-gap',
         name: 'grid-gap',
         matcher: 'Gg',
