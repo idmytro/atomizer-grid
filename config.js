e0e03661d64d787319a0ce5ffda58ff2df32574f
Dmytro Cisar <i@dmyt.ro>
2018-02-04 00:03:45 +0200

    + Example 8

--- config.js
+++ config.js
@@ -3,10 +3,20 @@
 const custom = {
     a: 'auto',
     g: 'grid',
-    span_2: 'span 2'
+    span_2: 'span 2',
+    span_3: 'span 3',
+    span_4: 'span 4',
+    col_2: 'col 2',
+    col_3: 'col 3',
+    col_4: 'col 4',
+    row_2: 'row 2',
+    row_3: 'row 3',
+    row_4: 'row 4',
 };
 
 const gridLineNames = [
+    'col',
+    'row',
     'col1-start',
     'col2-start',
     'col3-start',
