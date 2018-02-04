58ee22954bcb91ccb1b900fb7c037d67d38619a7
Dmytro Cisar <i@dmyt.ro>
2018-02-05 00:15:08 +0200

    don't use a: 'auto' in rules

--- rules.js
+++ rules.js
@@ -37,10 +37,7 @@ module.exports = [
         allowParamToValue: true,
         styles: {
             'grid-template-columns': '$0 $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11',
-        },
-        arguments: [{
-            'a': 'auto'
-        }]
+        }
     },
     {
         type: 'pattern',
