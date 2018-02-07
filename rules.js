1fbe15b39d1887db96a914b29b455f651956ecc5
Dmytro Cisar <i@dmyt.ro>
2018-02-08 00:28:10 +0200

    Gac arguments

--- rules.js
+++ rules.js
@@ -31,13 +31,18 @@ module.exports = [
     },
     {
         type: 'pattern',
-        id: 'grid-auto-column',
-        name: 'grid-auto-column',
+        id: 'grid-auto-columns',
+        name: 'grid-auto-columns',
         matcher: 'Gac',
         allowParamToValue: true,
         styles: {
             'grid-auto-columns': '$0',
-        }
+        },
+        arguments: [{
+            'a': 'auto',
+            'maxc': 'max-content',
+            'minc': 'min-content',
+        }]
     },
     {
         type: 'pattern',
